746e73a4a3750ca0e32461cad202d7b9b8d9f91e
Keks <keks@htmlacademy.ru>
2022-10-16 16:43:13 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,52 @@
+const getRandomPositiveInteger = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const createRandomUrlFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+
+    previousValues.push(currentValue);
+    return `photos/${currentValue}.jpg`;
+  };
+};
+
+const rangeLikes = (start, end) => Array(end - start + 1).fill().map((_, idx) => start + idx);
+
+const checkerLengthStr = (str, maxLength) => str.length <= maxLength;
+
+export {getRandomArrayElement, createRandomIdFromRangeGenerator, createRandomUrlFromRangeGenerator, checkerLengthStr, rangeLikes};
