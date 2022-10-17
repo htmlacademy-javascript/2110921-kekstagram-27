d34f7c7b04a268870ccabaf561553466d8ed1a14
Keks <keks@htmlacademy.ru>
2022-10-17 22:18:39 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/fullPhoto.js
@@ -0,0 +1,61 @@
+const body = document.querySelector('body');
+const bigPictureElement = document.querySelector('.big-picture');
+const previewPhoto = bigPictureElement.querySelector('.big-picture__img');
+const previewPhotoImage = previewPhoto.querySelector('img');
+const likesCounter = bigPictureElement.querySelector('.likes-count');
+const commentsCounter = bigPictureElement.querySelector('.comments-count');
+const socialCaption = bigPictureElement.querySelector('.social__caption');
+const socialCommentList = bigPictureElement.querySelector('.social__comments');
+const socialCommentsCounter = document.querySelector('.social__comment-count');
+const loadButton = document.querySelector('.comments-loader');
+const closePicture = bigPictureElement.querySelector('#picture-cancel');
+
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+
+const makeSocialCommentItem = (comments) => {
+  comments.forEach(({avatar, name, message}) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    commentElement.querySelector('img').src = avatar;
+    commentElement.querySelector('img').alt = name;
+    commentElement.querySelector('.social__text').textContent = message;
+    socialCommentList.append(commentElement);
+  });
+};
+
+const makeBigPicture = ({url, likes, comments, description}) => {
+  previewPhotoImage.src = url;
+  likesCounter.textContent = likes;
+  commentsCounter.textContent = comments.length;
+  makeSocialCommentItem(comments);
+  socialCaption.textContent = description;
+};
+
+const onEscClose = (evt) => {
+  if (evt.keyCode === 27) {
+    bigPictureElement.classList.add('hidden');
+  }
+};
+
+const openCard = (card) => {
+  bigPictureElement.classList.remove('hidden');
+  socialCommentsCounter.classList.add('hidden');
+  loadButton.classList.add('hidden');
+  body.classList.add('modal-open');
+
+  makeBigPicture(card);
+  document.body.addEventListener('keydown', onEscClose);
+};
+
+const closeCard = () => {
+  bigPictureElement.classList.add('hidden');
+  socialCommentsCounter.classList.remove('hidden');
+  loadButton.classList.remove('hidden');
+  body.classList.remove('modal-open');
+  socialCommentList.innerHTML = '';
+};
+
+closePicture.addEventListener('click',() => {
+  closeCard ();
+});
+
+export {openCard};
---
+++ 7/js/photo.js
@@ -0,0 +1,21 @@
+import {CreateCardCollection} from './data.js';
+import {openCard} from './fullPhoto.js';
+
+const userPictureList = document.querySelector('.pictures');
+const similarCardTemplate = document.querySelector('#picture').content.querySelector('a');
+
+const similarCards = CreateCardCollection();
+
+const pictureListFragment = document.createDocumentFragment();
+
+similarCards.forEach((card) => {
+  const cardElement = similarCardTemplate.cloneNode(true);
+  cardElement.querySelector('img').src = card.url;
+  cardElement.querySelector('.picture__likes').textContent = card.likes;
+  cardElement.querySelector('.picture__comments').textContent = card.comments.length;
+  userPictureList.append(cardElement);
+
+  cardElement.addEventListener('click', () => openCard(card));
+});
+
+userPictureList.append(pictureListFragment);
