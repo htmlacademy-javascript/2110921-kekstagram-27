d79a3ed8b9b18e6c04655ddbe40c5d870569750f
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2022-11-04 16:08:35 +0300

    Убрала ошибку в адресе сервера для отправки

--- js/api-server.js
+++ js/api-server.js
@@ -11,7 +11,7 @@ const getData = (onSuccess, onError) => {
 
 const sendData = (onSuccess, onFail, body) => {
   fetch(
-    'https://27.javascript.pages.academ/kekstagram',
+    'https://27.javascript.pages.academy/kekstagram',
     {
       method: 'POST',
       body,
